451f2c3d7065bff1e006df74ae1b9540c95151c1
Prdevoney <prdevo@gmail.com>
2025-04-07 21:45:16 -0400

    created structure for postgreSQL and created triggers

---
+++ college_event_schema.sql
@@ -0,0 +1,72 @@
+CREATE TABLE universities (
+    university_id SERIAL PRIMARY KEY, 
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    description TEXT, 
+    email_domain VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE locations (
+    location_id SERIAL PRIMARY KEY,
+    name VARCHAR (255) NOT NULL,
+    address VARCHAR (255) NOT NULL,
+    longitude FLOAT NOT NULL,
+    latitude FLOAT NOT NULL,
+    UNIQUE (longitude, latitude)
+);
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    university_id INTEGER REFERENCES universities (university_id),
+    user_type VARCHAR(50) NOT NULL CHECK (user_type IN ('student', 'admin', 'super_admin'))
+);
+
+CREATE TABLE rso (
+    rso_id SERIAL PRIMARY KEY, 
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    admin_id INTEGER NOT NULL REFERENCES users (user_id),
+    university_id INTEGER NOT NULL REFERENCES universities (university_id),
+    status VARCHAR(50) DEFAULT 'inactive' NOT NULL CHECK (status IN ('active', 'inactive'))
+); 
+
+CREATE TABLE rso_members (
+    user_id INTEGER NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
+    rso_id INTEGER NOT NULL REFERENCES rso (rso_id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, rso_id)
+);
+
+CREATE TABLE events (
+    event_id SERIAL PRIMARY KEY, 
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    description TEXT,
+    event_date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    location_id INTEGER NOT NULL REFERENCES locations (location_id),
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    created_by INTEGER NOT NULL REFERENCES users (user_id),
+    event_type VARCHAR(50) NOT NULL CHECK (event_type IN ('public', 'private', 'ros')),
+    university_id INTEGER REFERENCES universities (university_id),
+    rso_id INTEGER REFERENCES rso (rso_id),
+    approved BOOLEAN DEFAULT FALSE,
+    CONSTRAINT event_type_check CHECK (
+        (event_type = 'public' AND university_id IS NOT NULL rso_id IS NULL) OR
+        (event_type = 'private' AND university_id IS NOT NULL rso_id IS NOT NULL) OR
+        (event_type = 'rso' AND university_id IS NOT NULL AND rso_id IS NOT NULL ) 
+    )
+);
+
+CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY, 
+    event_id INTEGER NOT NULL REFERENCES events (event_id) ON DELETE CASCADE,
+    user_id INTEGER NOT NULL REFERENCES users (user_id),
+    comment_text TEXT,
+    rating INTEGER CHECK (rating >= 1 AND rating <= 5)
+);
---
+++ college_event_triggers.sql
@@ -0,0 +1,78 @@
+-- Trigger to update the status of an RSO when a member is added or removed
+CREATE FUNCTION update_rso_status() RETURNS TRIGGER AS $$
+BEGIN 
+    -- check if insert or deletion
+    IF (TG_OP = 'INSERT') THEN 
+        -- check if RSO now has >=5 members
+        IF ((SELECT COUNT(*) FROM rso_members WHERE rso_id = NEW.rso_id) >= 5) THEN 
+            -- update RSO status to active
+            UPDATE rso SET status = 'active' WHERE rso_id = NEW.rso_id;
+        END IF;
+        RETURN NEW; 
+    ELSIF (TG_OP = 'DELETE') THEN
+        -- check if RSO now has <5 members
+        IF ((SELECT COUNT(*) FROM rso_members WHERE rso_id = OLD.rso_id) < 5) THEN 
+            -- update RSO status to inactive
+            UPDATE rso SET status = 'inactive' WHERE rso_id = OLD.rso_id;
+        END IF;
+        RETURN OLD; 
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Triggers to update RSO status when a member is added or removed
+CREATE TRIGGER ros_member_added 
+AFTER INSERT ON rso_members
+FOR EACH ROW
+EXECUTE FUNCTION update_rso_status();
+
+CREATE TRIGGER ros_member_removed
+AFTER DELETE ON rso_members
+FOR EACH ROW
+EXECUTE FUNCTION update_rso_status();
+
+-- Overlapping events
+CREATE FUNCTION check_event_overlap() RETURNS TRIGGER AS $$ 
+BEGIN 
+    -- check if event overlaps with existing event
+    IF EXISTS (
+        SELECT 1 FROM events
+        WHERE location_id = NEW.location_id 
+        AND event_date = NEW.event_date
+        AND (
+            (NEW.start_time < end_time AND NEW.end_time > start_time)
+        )
+    ) THEN 
+        RAISE EXCEPTION 'Event overlaps with an existing event at the same location.';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER event_overlap_check 
+BEFORE insert OR UPDATE ON events 
+FOR EACH ROW 
+EXECUTE FUNCTION check_event_overlap();
+
+-- Trigger to make sure only an admin can create an RSO event
+CREATE FUNCTION check_rso_admin() RETURNS TRIGGER AS $$
+BEGIN 
+    -- check if this is an RSO event
+    IF NEW.event_type = 'rso' AND NEW.rso_id IS NOT NULL THEN 
+        -- check if the user is an admin of the RSO
+        IF NOT EXISTS (
+            SELECT 1 FROM rso_members 
+            WHERE rso_id = NEW.rso_id 
+            AND admin_id = (SELECT user_id FROM users WHERE user_id = NEW.created_by)
+        ) THEN 
+            RAISE EXCEPTION 'User is not an admin of the RSO.';
+        END IF;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_rso_admin_trigger
+BEFORE INSERT OR UPDATE ON events
+FOR EACH ROW
+EXECUTE FUNCTION check_rso_admin();
